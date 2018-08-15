f126d336b84a85268fe89aa45b43984f7b2553e5
Venkatesh Prasad <prasad.venkatesh90@gmail.com>
2018-08-16 00:53:43 +0530

    Update views.py

--- app/views.py
+++ app/views.py
@@ -3,6 +3,7 @@ from django.template import loader
 from django.http import JsonResponse
 from django.http import HttpResponse
 import diff_match_patch
+import json
 # Create your views here.
 def home(request):
     return render(request,'app/index.html')  
@@ -38,19 +39,26 @@ def diffMatchPatch(request):
         dmp = diff_match_patch.diff_match_patch()
         dmp.Diff_Timeout = 0   # or some other value, default is 1.0 seconds
         diffs = dmp.diff_main(str1, str2)
-        html = ""
-        for i in diffs:
+        diffs_list = []
+        for i in range(len(diffs)):
+            diffs_list.append(list(diffs[i]))
+        
+        """for i in diffs:
             if i[0] == 0:
-                html += i[1]+"<br><br>"
-                
+                same.append(i[1])    
+                        
             if i[0] == -1:
-                html += """<strike>"""+i[1]+"""</strike><br><br>
-                <button type="button" class="btn btn-success">Accept</button>
-                <button type="button" class="btn btn-danger">Reject</button><br>"""   
-                
+                removed.append(i[1])   
+                        
             if i[0] == 1:
-                html += """<u>"""+i[1]+"""</u><br><br>
-                <button type="button" class="btn btn-success">Accept</button>
-                <button type="button" class="btn btn-danger">Reject</button><br>"""
-        print(html)
-        return HttpResponse(html)
+                added.append(i[1])
+        #print(same)
+        #print(removed)
+        #print(added)
+        """
+        dictt = {}
+        dictt = {'diffs':diffs_list}
+        #print(dictt)
+        json_data = json.dumps(dictt)
+        print(json_data)
+        return JsonResponse(json_data, safe = False)
