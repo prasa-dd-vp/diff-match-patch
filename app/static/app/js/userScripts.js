343d1181a08351b42951e313db44ec62fc227d62
Venkatesh Prasad <prasad.venkatesh90@gmail.com>
2018-08-16 15:28:11 +0530

    Update userScripts.js
    added add() function

--- app/static/app/js/userScripts.js
+++ app/static/app/js/userScripts.js
@@ -1,4 +1,6 @@
+var removed_string = '';
 var len = 0;
+var para1, para2;
 function remove(i,j)
 {
 	
@@ -6,14 +8,14 @@ console.log(i);
 console.log(j);
 var ifrm = parent.document.getElementById('doc1');
 var doc = ifrm.contentDocument? ifrm.contentDocument: ifrm.contentWindow.document;
-var str = doc.getElementById("target1").innerHTML;
-	
-
-doc.getElementById("target1").innerHTML = str.slice(0,i-len)+str.slice(j-len);
+var get_str = doc.getElementById("target1").innerHTML;
+var set_str = get_str.slice(0,i)+get_str.slice(j)	
+removed_string = set_str;
+doc.getElementById("target1").innerHTML = set_str;
 //console.log(str.slice(0,len-i)+str.slice(len-j));
 document.getElementById("But"+i+"").style.visibility = "hidden";
 
-len = len+(j-i);
+//len = len+(j-i);
 
 /*//console.log(doc.getElementById('target1'));
 var str = doc.getElementById("target1").innerHTML;
@@ -22,8 +24,23 @@ console.log(str.slice(0,i)+str.slice(j));
 doc.getElementById("target1").innerHTML = str;*/
 }
 
+function add(i,strr){
+	//alert("haaan okay");
+	//console.log(i);
+	//console.log(str);
+	var ifrm = parent.document.getElementById('doc1');
+	var doc = ifrm.contentDocument? ifrm.contentDocument: ifrm.contentWindow.document;
+	var get_str = doc.getElementById("target1").innerHTML;
+	doc.getElementById("target1").innerHTML = get_str.slice(0,i)+strr+get_str.slice(i);
+	removed_string = get_str.slice(0,i)+strr+get_str.slice(i);
+	//console.log(get_str);
+	//console.log(get_str.slice(0,i) + strr + get_str.slice(i));
+	document.getElementById("add"+i+"").style.visibility = "hidden";
+
+}
+
 $(document).ready(function(){
-	var para1, para2;
+	//var para1, para2;
 	var diffs_array = []
 	var html_string = '';
 
@@ -84,13 +101,14 @@ $(document).ready(function(){
 		var json = JSON.parse(data);
 		diffs_array = json['diffs'];
 		
-		var removed_string = '';
+		
 		var added_string = '';
 		$.each(diffs_array, function(index, array) { // This each iterates over the arrays.
 			
 			if (array[0] == 0){
 			html_string += array[1]+"<br>";
-			removed_string += array[1]
+			removed_string += array[1];
+			added_string += array[1];
 			}
 			
 			if (array[0] == -1){
@@ -100,11 +118,13 @@ $(document).ready(function(){
 			}
 			
 			if (array[0] == 1){
-			html_string += "<u>"+array[1]+`</u><br>`;
+				var ar = array[1];
+			html_string += "<u>"+array[1]+`</u><br><button class="add"  id="add`+added_string.length+`" onclick = "add(`+parseInt(added_string.length)+`,'`+ar+`')">Accept</button><br>`;
 			added_string += array[1];
+			//console.log(html_string)
 			}
 		});
-		//console.log(string)
+		
 		$('#resultant').contents().find('#result').html(html_string);
 	}
 	
